a61989425b3f1f584179265e239361736554528a
Tom Dwyer <tom.dwyer@trivago.com>
2017-07-21 18:26:50 +0200

    Built rough flexbox structure

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,6 @@
 import { createComponent, RECEIVE_PROPS } from 'melody-component';
 import { bindEvents } from 'melody-hoc';
 import template from './index.twig';
-import './index.scss';
 
 const initialState = {};
 
